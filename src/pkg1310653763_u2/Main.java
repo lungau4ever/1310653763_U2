40fcb0cbd7160a7dcb4663124355a4df1d12c935
xxx <xxx@xxx-PC>
2014-05-06 21:38:00 +0200

    erster testcommit

---
+++ src/pkg1310653763_u2/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg1310653763_u2;
+
+/**
+ *
+ * @author Manuel Dorfer
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
